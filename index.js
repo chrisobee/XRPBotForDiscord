8c4d548bbb2506145804fe3285856ac3deee581d
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-02 12:29:46 -0600

    Added max and min notify functionality

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
-
 import Discord from "discord.js";
 import axios from 'axios';
+
+
 const botToken = process.env.BOT_TOKEN;
 const apiKey = process.env.NOMICS_KEY;
 const client = new Discord.Client();
@@ -20,10 +21,9 @@ function displayCurrentPrice(msg){
         .catch(err=>console.log(err));
 }
 
-function checkPrice(){
-    const maxPrice = 1.00;
-    const minPrice = .24;
+function checkPrice(maxToNotify, minToNotify){
     const channelId = '805947280725114921'
+
     getXrpData()
         .then(data=>{
             //Displays current price to log
@@ -31,11 +31,11 @@ function checkPrice(){
             console.log(`Current Price: $${currentPrice}`);
 
             //Checks if the current price is above a certain set level or below a certain set level
-            if(currentPrice > maxPrice){
+            if(currentPrice > maxToNotify){
                 client.channels.cache.get(channelId)
                 .send(`The price of XRP is above $${maxPrice}!!! SELL NOW!`);
             }
-            else if(currentPrice < minPrice){
+            else if(currentPrice < minToNotify){
                 client.channels.cache.get(channelId)
                 .send(`The price of XRP is below $${minPrice}!! WATCH OUT!!!!`);
             }
@@ -43,17 +43,44 @@ function checkPrice(){
         .catch(err=>console.log(err));
 }
 
+function setNotify(msg, type){
+    msg.reply(`What would you like to set the ${type} to?`);
+    const checkInput = m => isFinite(m.content);
 
+    const collector = message.channel.createMessageCollector(checkInput, {time: 15000});
 
+    collector.on('collect', m => {
+        var notifyValue = Number.parseFloat(m.content);
+        return notifyValue;
+    });
+
+    collector.on('end', collected => {
+        console.log(`Collected ${collected.size} items`);
+        return NaN;
+    });
+}
+
+//Verifies Bot is Operational
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//Basic Functionality to Check XRP Price
 client.on('message', msg => {
     if(msg.content === 'xrp-price' || msg.content === 'xrp-p'){
         displayCurrentPrice(msg);
     }
 });
 
+client.on('message', msg => {
+    var maxToNotify = NaN;
+    var minToNotify = NaN;
+    if(msg.content.startsWith('xrp-notify') || msg.content.startsWith(`xrp-n`)){
+        maxToNotify = setNotify(msg, 'max');
+        minToNotify = setMin(msg, 'min');
+    }
+
+    setInterval(checkPrice(maxToNotify, minToNotify), 30000);
+})
+
 client.login(botToken);
-setInterval(checkPrice, 30000);
