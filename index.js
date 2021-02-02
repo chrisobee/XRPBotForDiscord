19523fb7fb9bcf817c782d30c23d7f669d162ad9
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-02 10:09:07 -0600

    Refactored various functions

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const botToken = process.env.BOT_TOKEN;
 const coinbaseKey = process.env.COINBASE_KEY;
 const client = new Discord.Client();
 
-function displayCurrentPrice(msg){
+function getXrpData(){
     const URL = 'https://rest.coinapi.io/v1/assets?filter_asset_id=XRP';
-        axios.get(URL, {
-            headers:{
-                'X-CoinAPI-Key':coinbaseKey
-            }  
-        })
+    return axios.get(URL, {
+        headers:{
+            'X-CoinAPI-Key': coinbaseKey
+        }
+    })
+}
+
+function displayCurrentPrice(msg){
+    getXrpData()
         .then(data=>{
             console.log(data.data[0]);
             msg.reply(`The current price of XRP is $${data.data[0]['price_usd'].toFixed(4)}`);
@@ -23,17 +27,13 @@ function displayCurrentPrice(msg){
 function checkPrice(){
     const maxPrice = 1.00;
     const minPrice = .24;
-
-    const URL = 'https://rest.coinapi.io/v1/assets?filter_asset_id=XRP';
-    
-        axios.get(URL, {
-            headers:{
-                'X-CoinAPI-Key':coinbaseKey
-            }  
-        })
+    getXrpData()
         .then(data=>{
+            //Displays current price to log
             const currentPrice = data.data[0]['price_usd'];
             console.log(`Current Price: $${currentPrice}`);
+
+            //Checks if the current price is above a certain set level or below a certain set level
             if(currentPrice > maxPrice){
                 client.channels.cache.get('805947280725114921')
                 .send(`The price of XRP is above $${maxPrice}!!! SELL NOW!`);
